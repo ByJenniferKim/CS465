097edb2d1b7651a9b85af9919bba92f661ceac27
Jennifer Kim <byjenniferkim@gmail.com>
2025-03-24 00:07:24 -0500

    Module 3 completed baseline

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 const crtlMain = require('../controllers/main');
+const ctrlTravel = require('../controllers/travel');
 
 /* GET home page. */
 router.get('/', crtlMain.index);
+
+/* GET travel page */
+router.get('/travel', ctrlTravel.travel);
 
 module.exports = router;
